f7991fd404afbd039c015eb04ee43b90963fa622
Serhii Tkachuk <sergiytkachuk29@gmail.com>
2023-01-14 22:35:51 +0200

    created functions to move the snake

--- script.js
+++ script.js
@@ -66,3 +66,35 @@ btnLevel.onclick = function () {
     jogo = setInterval(iniciarJogo, vel);
   }
 };
+
+function criarBG() {
+  context.fillStyle = "lightgreen";
+  context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha() {
+  for (i = 0; i < snake.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+    context.strokeStyle = "lightgreen";
+    context.strokeRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
+function drawFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
+  context.strokeStyle = "lightgreen";
+  context.strokeRect(food.x, food.y, box, box);
+}
+
+document.addEventListener("keydown", update);
+
+function update(event) {
+  if (!fim) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+  }
+}
